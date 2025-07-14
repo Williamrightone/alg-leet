68568f2273b75f740243848eb8677edb38fcbf28
william <willy4543@gmail.com>
2025-07-14 13:16:48 +0800

    top150 24

--- src/main/java/com/william/top150/twopointer/IsSubsequence.java
+++ src/main/java/com/william/top150/twopointer/IsSubsequence.java
@@ -1,2 +1,57 @@
-package com.william.top150.twopointer;public class IsSubsequence {
+package com.william.top150.twopointer;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class IsSubsequence {
+
+    public static void main(String[] args) {
+
+        String s = "abc";
+
+        String t = "ahbgdc";
+
+        System.out.println(isSubsequence(s,t));
+
+    }
+
+    public static boolean isSubsequence(String s, String t) {
+
+        Queue<Character> queue = new LinkedList<>();
+
+        for (char c : s.toCharArray()) {
+            queue.add(c);
+        }
+
+        for (char c : t.toCharArray()) {
+            if (!queue.isEmpty() && c == queue.peek()) {
+                queue.poll();
+            }
+        }
+
+        return queue.isEmpty();
+
+    }
+
+    /**
+     * two point
+     * @param s
+     * @param t
+     * @return
+     */
+    public static boolean isSubsequence2(String s, String t) {
+
+        int i =0, j =0;
+
+        while(i < s.length() && j < t.length()){
+
+            if(s.charAt(i) == t.charAt(j)){
+                i++;
+            }
+            j++;
+        }
+
+        return i == s.length();
+    }
+
 }
